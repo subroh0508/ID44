dae99cff8dae61ab61112fc746eff7d4303fc9f9
subroh0508 <subroh.0508@gmail.com>
2019-11-09 11:26:22 +0900

    [edit]Move main -> androidMain in MPP modules

--- buildSrc/src/main/java/AndroidBuildExtensions.kt
+++ buildSrc/src/main/java/AndroidBuildExtensions.kt
@@ -1,6 +1,76 @@
 import com.android.build.gradle.BaseExtension
+import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
+import org.gradle.api.JavaVersion
 import org.gradle.api.Project
+import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.gradle.api.plugins.ExtensionAware
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
+import org.jetbrains.kotlin.gradle.plugin.KotlinDependencyHandler
+
+internal fun DependencyHandler.implementation(dependency: Any) {
+    add("implementation", dependency)
+}
+
+internal fun DependencyHandler.testImplementation(dependency: Any) {
+    add("testImplementation", dependency)
+}
+
+internal fun DependencyHandler.androidTestImplementation(dependency: Any) {
+    add("androidTestImplementation", dependency)
+}
+
+internal fun DependencyHandler.kapt(dependency: Any) {
+    add("kapt", dependency)
+}
 
 fun Project.androidExt(configure: BaseExtension.() -> Unit) =
     (this as ExtensionAware).extensions.configure("android", configure)
+
+internal fun Project.androidShared() = androidExt {
+    compileSdkVersion(Package.Versions.compileSdk)
+
+    defaultConfig {
+        minSdkVersion(Package.Versions.minSdk)
+        targetSdkVersion(Package.Versions.targetSdk)
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+fun Project.androidCommons() {
+    androidShared()
+    androidExt {
+        sourceSets.forEach {
+            it.java.setSrcDirs(files("src/${it.name}/kotlin"))
+        }
+    }
+}
+
+fun Project.androidMPP() {
+    androidShared()
+    androidExt {
+        sourceSets.forEach {
+            if (it.name.startsWith("androidTest")) {
+                return@forEach
+            }
+            it.java.setSrcDirs(files("src/android${it.name.toUpperFirstChar()}/kotlin"))
+            it.manifest.srcFile("src/androidMain/AndroidManifest.xml")
+        }
+    }
+}
+
+private fun String.toUpperFirstChar() = when (length) {
+    0 -> ""
+    1 -> toUpperCase()
+    else -> get(0).toUpperCase() + substring(1)
+}
--- libraries/timeline/domain/entity/build.gradle.kts
+++ libraries/timeline/domain/entity/build.gradle.kts
@@ -3,29 +3,7 @@ plugins {
     id("com.android.library")
 }
 
-android {
-    compileSdkVersion(Package.Versions.compileSdk)
-
-    defaultConfig {
-        minSdkVersion(Package.Versions.minSdk)
-        targetSdkVersion(Package.Versions.targetSdk)
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-
-    sourceSets.forEach {
-        it.java.setSrcDirs(files("src/${it.name}/kotlin"))
-    }
-}
+androidMPP()
 
 kotlin {
     android()
