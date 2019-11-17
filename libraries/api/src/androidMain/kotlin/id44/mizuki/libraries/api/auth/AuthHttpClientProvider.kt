2dd7511a5d0af676f09faaf4dbe8d1b4b3267844
subroh0508 <subroh.0508@gmail.com>
2019-11-18 00:04:59 +0900

    [edit]Create AuthHttpClientProvider

---
+++ libraries/api/src/androidMain/kotlin/id44/mizuki/libraries/api/auth/AuthHttpClientProvider.kt
@@ -0,0 +1,39 @@
+package id44.mizuki.libraries.api.auth
+
+import id44.mizuki.libraries.api.BuildConfig
+import id44.mizuki.libraries.api.auth.model.AccessToken
+import id44.mizuki.libraries.api.auth.model.AppCredential
+import id44.mizuki.libraries.api.auth.params.PostApps
+import id44.mizuki.libraries.api.auth.params.PostOauthToken
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.features.DefaultRequest
+import io.ktor.client.features.UserAgent
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import io.ktor.http.userAgent
+import kotlinx.serialization.json.Json
+import okhttp3.logging.HttpLoggingInterceptor
+
+internal object AuthHttpClientProvider {
+    fun provide(userAgent: UserAgent, json: Json): HttpClient = HttpClient(OkHttp) {
+        engine {
+            if (BuildConfig.DEBUG) {
+                val loggingInterceptor = HttpLoggingInterceptor()
+                loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+                addInterceptor(loggingInterceptor)
+            }
+        }
+        install(DefaultRequest) {
+            userAgent(userAgent.agent)
+        }
+        install(JsonFeature) {
+            serializer = KotlinxSerializer(json).apply {
+                setMapper(PostApps.Request::class, PostApps.Request.serializer())
+                setMapper(PostOauthToken.Request::class, PostOauthToken.Request.serializer())
+                setMapper(AccessToken::class, AccessToken.serializer())
+                setMapper(AppCredential::class, AppCredential.serializer())
+            }
+        }
+    }
+}
--- libraries/api/src/androidMain/kotlin/id44/mizuki/libraries/api/auth/di/MastodonAuthApiModule.kt
+++ libraries/api/src/androidMain/kotlin/id44/mizuki/libraries/api/auth/di/MastodonAuthApiModule.kt
@@ -7,6 +7,7 @@ import dagger.Provides
 import id44.mizuki.base.scope.ModuleScope
 import id44.mizuki.libraries.api.PrefKeys.NAME_ACCESS_TOKEN_PREFERENCES
 import id44.mizuki.libraries.api.PrefKeys.NAME_APP_CREDENTIAL_PREFERENCES
+import id44.mizuki.libraries.api.auth.AuthHttpClientProvider
 import id44.mizuki.libraries.api.auth.client.AppCredentialStore
 import id44.mizuki.libraries.api.auth.client.AppCredentialStoreClient
 import id44.mizuki.libraries.api.auth.client.MastodonAuthApi
@@ -14,21 +15,23 @@ import id44.mizuki.libraries.api.auth.client.MastodonAuthApiClient
 import id44.mizuki.libraries.api.client.AccessTokenStore
 import id44.mizuki.libraries.api.client.AccessTokenStoreClient
 import io.ktor.client.HttpClient
+import io.ktor.client.features.UserAgent
+import kotlinx.serialization.json.Json
 
 @Module
 class MastodonAuthApiModule {
     @Provides
     @ModuleScope
-    fun provideAppCredentialStore(app: Application): AppCredentialStore
-            = AppCredentialStoreClient(app.getSharedPreferences(NAME_APP_CREDENTIAL_PREFERENCES, Context.MODE_PRIVATE))
+    fun provideAppCredentialStore(app: Application): AppCredentialStore =
+        AppCredentialStoreClient(app.getSharedPreferences(NAME_APP_CREDENTIAL_PREFERENCES, Context.MODE_PRIVATE))
 
     @Provides
     @ModuleScope
-    fun provideAccessTokenStore(app: Application): AccessTokenStore
-            = AccessTokenStoreClient(app.getSharedPreferences(NAME_ACCESS_TOKEN_PREFERENCES, Context.MODE_PRIVATE))
+    fun provideAccessTokenStore(app: Application): AccessTokenStore =
+        AccessTokenStoreClient(app.getSharedPreferences(NAME_ACCESS_TOKEN_PREFERENCES, Context.MODE_PRIVATE))
 
     @Provides
     @ModuleScope
-    fun provideMastodonAuthApi(httpClient: HttpClient): MastodonAuthApi
-            = MastodonAuthApiClient(httpClient)
+    fun provideMastodonAuthApi(userAgent: UserAgent, json: Json): MastodonAuthApi =
+        MastodonAuthApiClient(AuthHttpClientProvider.provide(userAgent, json))
 }
