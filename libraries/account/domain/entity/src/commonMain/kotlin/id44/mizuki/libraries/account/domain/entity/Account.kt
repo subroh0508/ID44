d5ad47b376afe63b80d9fa8e6b927ffdb1b6ba3d
subroh0508 <subroh.0508@gmail.com>
2019-11-30 18:16:57 +0900

    [edit]Change Account identifier String to AccountId

--- libraries/account/domain/entity/src/commonMain/kotlin/id44/mizuki/libraries/account/domain/entity/Account.kt
+++ libraries/account/domain/entity/src/commonMain/kotlin/id44/mizuki/libraries/account/domain/entity/Account.kt
@@ -1,9 +1,10 @@
 package id44.mizuki.libraries.account.domain.entity
 
+import id44.mizuki.libraries.shared.valueobject.AccountId
 import id44.mizuki.libraries.shared.valueobject.HostName
 
 data class Account(
-    val id: String,
+    val id: AccountId,
     val username: String,
     val displayName: String,
     val hostName: HostName
