42182fe4c153276034983de708cfd3ff3787cc12
subroh0508 <subroh.0508@gmail.com>
2019-11-30 18:44:12 +0900

    [edit]Use inline classes on :libaries:timeline:domain:usecase:unsubscribe module

--- libraries/timeline/domain/usecase/unsubscribe/src/commonMain/kotlin/id44/mizuki/libraries/timeline/domain/unsubscribe/TimelineUnsubscribeUseCase.kt
+++ libraries/timeline/domain/usecase/unsubscribe/src/commonMain/kotlin/id44/mizuki/libraries/timeline/domain/unsubscribe/TimelineUnsubscribeUseCase.kt
@@ -1,7 +1,9 @@
 package id44.mizuki.libraries.timeline.domain.unsubscribe
 
+import id44.mizuki.libraries.shared.valueobject.AccountId
+import id44.mizuki.libraries.shared.valueobject.HostName
 import id44.mizuki.libraries.timeline.domain.valueobject.Stream
 
 interface TimelineUnsubscribeUseCase {
-    fun execute(hostName: String, stream: Stream)
+    fun execute(hostName: HostName, id: AccountId, stream: Stream)
 }
--- libraries/timeline/domain/usecase/unsubscribe/src/commonMain/kotlin/id44/mizuki/libraries/timeline/domain/unsubscribe/TimelineUnsubscribeUseCaseImpl.kt
+++ libraries/timeline/domain/usecase/unsubscribe/src/commonMain/kotlin/id44/mizuki/libraries/timeline/domain/unsubscribe/TimelineUnsubscribeUseCaseImpl.kt
@@ -1,12 +1,13 @@
 package id44.mizuki.libraries.timeline.domain.unsubscribe
 
+import id44.mizuki.libraries.shared.valueobject.AccountId
+import id44.mizuki.libraries.shared.valueobject.HostName
 import id44.mizuki.libraries.timeline.domain.valueobject.Stream
 import id44.mizuki.libraries.timeline.infra.repository.StatusRepository
 
 internal class TimelineUnsubscribeUseCaseImpl(
     private val repository: StatusRepository
 ) : TimelineUnsubscribeUseCase {
-    override fun execute(hostName: String, stream: Stream) {
-        repository.closeSubscription(hostName, stream)
-    }
+    override fun execute(hostName: HostName, id: AccountId, stream: Stream) =
+        repository.closeSubscription(hostName, id, stream)
 }
