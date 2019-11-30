edff9fa9e914b02459cb0e5888a22f6b34c77d2a
subroh0508 <subroh.0508@gmail.com>
2019-12-01 03:07:49 +0900

    [update]Update version on buildSrc's build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:3.4.2")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
+    implementation("com.android.tools.build:gradle:3.5.2")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
 }
