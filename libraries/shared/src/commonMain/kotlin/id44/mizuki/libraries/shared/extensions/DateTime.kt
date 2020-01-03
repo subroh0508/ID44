ff4fed174455d5ee21a613e6a0b3240d6f44ff96
subroh0508 <subroh.0508@gmail.com>
2020-01-03 15:28:32 +0900

    [edit]Add DateTime extension method

--- libraries/shared/src/commonMain/kotlin/id44/mizuki/libraries/shared/extensions/DateTime.kt
+++ libraries/shared/src/commonMain/kotlin/id44/mizuki/libraries/shared/extensions/DateTime.kt
@@ -7,5 +7,7 @@ import com.soywiz.klock.parse
 fun String.toDateTimeTz() = DateFormat(DATE_TIME_TZ_PATTERN).parse(this)
 
 fun DateTimeTz.toDateTimeTzFormat() = format(DATE_TIME_TZ_PATTERN)
+fun DateTimeTz.toDate() = format(DATE_PATTERN)
 
 internal const val DATE_TIME_TZ_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"
+internal const val DATE_PATTERN = "yyyy/MM/dd"
