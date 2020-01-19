2e8e1690d011dc0b357eadb3458d2032f99fc150
subroh0508 <subroh.0508@gmail.com>
2020-01-20 02:33:27 +0900

    [edit]Move @font-face style to index.html

--- web/index.js
+++ web/index.js
@@ -2,27 +2,5 @@ import React from 'react';
 import { AppRegistry } from 'react-native';
 import { registerComponent } from 'ID44-frontend/components/signin';
 
-// Use prebuilt version of RNVI in dist folder
-import Icon from 'react-native-vector-icons/dist/FontAwesome';
-
-// Generate required css
-import iconFont from 'react-native-vector-icons/Fonts/FontAwesome.ttf';
-const iconFontStyles = `@font-face {
-  src: url(${iconFont});
-  font-family: FontAwesome;
-}`;
-
-// Create stylesheet
-const style = document.createElement('style');
-style.type = 'text/css';
-if (style.styleSheet) {
-  style.styleSheet.cssText = iconFontStyles;
-} else {
-  style.appendChild(document.createTextNode(iconFontStyles));
-}
-
-// Inject stylesheet
-document.head.appendChild(style);
-
 registerComponent();
 AppRegistry.runApplication('SignIn', { rootTag: document.getElementById('app') });
