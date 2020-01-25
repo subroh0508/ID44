498b5692887be5a1ae8806978ad5af638af1c8df
subroh0508 <subroh.0508@gmail.com>
2020-01-25 22:17:29 +0900

    [delete]Remove web directory

--- web/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { AppRegistry } from 'react-native';
-import { registerComponent } from 'ID44-frontend/components/signin';
-
-registerComponent();
-AppRegistry.runApplication('SignIn', { rootTag: document.getElementById('app') });
