a449daf407122f2722d800f8283359d81356f6dd
subroh0508 <subroh.0508@gmail.com>
2020-02-06 00:12:28 +0900

    [add]Add LocalPreferences to :libraries:api

---
+++ libraries/api/src/jsMain/kotlin/id44/mizuki/libraries/api/LocalPreferences.kt
@@ -0,0 +1,73 @@
+package id44.mizuki.libraries.api
+
+import kotlinx.serialization.DeserializationStrategy
+import kotlinx.serialization.SerializationStrategy
+import kotlinx.serialization.json.Json
+
+@JsModule("fs")
+private external object fs {
+    fun readFileSync(path: String): String
+    fun writeFileSync(path: String, text: String)
+}
+
+private external fun delete(obj: dynamic): Boolean = definedExternally
+
+actual class LocalPreferences(
+    private val app: dynamic,
+    private val json: Json
+) {
+    private val data: dynamic
+    private val userDataPath get() = "${app.getPath("userData")}/id44_pref.json"
+
+    init {
+        data = try {
+            JSON.parse(fs.readFileSync(userDataPath))
+        } catch (e: Throwable) {
+            js("{}")
+        }
+    }
+
+    actual fun putBoolean(key: String, value: Boolean) { put(key, value) }
+    actual fun putString(key: String, value: String) { put(key, value) }
+    actual fun putStringSet(key: String, value: Set<String>) { put(key, JSON.stringify(value.toArray())) }
+    actual fun putInt(key: String, value: Int) { put(key, value) }
+    actual fun putLong(key: String, value: Long) { put(key, value) }
+    actual fun putFloat(key: String, value: Float) { put(key, value) }
+    actual fun <T: Any> put(key: String, strategy: SerializationStrategy<T>, value: T) {
+        put(key, json.stringify(strategy, value))
+    }
+
+    actual fun getBoolean(key: String, defValue: Boolean): Boolean = get(key) ?: defValue
+    actual fun getString(key: String, defValue: String?): String? = get(key) ?: defValue
+    actual fun getStringSet(key: String, defValue: Set<String>?): Set<String>? = (data[key] as? String)?.let {
+        (JSON.parse(it) as Array<String>).toSet()
+    }
+    actual fun getInt(key: String, defValue: Int): Int = get(key) ?: defValue
+    actual fun getLong(key: String, defValue: Long): Long = get(key) ?: defValue
+    actual fun getFloat(key: String, defValue: Float): Float = get(key) ?: defValue
+    actual fun <T: Any> get(key: String, strategy: DeserializationStrategy<T>): T? = (data[key] as? String)?.let {
+        json.parse(strategy, it)
+    }
+
+    actual fun remove(key: String) { delete(data[key]) }
+
+    private fun <E: Any> Set<E>.toArray(): Any = fold(js("[]")) { acc: dynamic, e: E ->
+        acc.push(e)
+        acc
+    } as Any
+    private fun <E: Any> toSet(array: dynamic): Set<E> = (array.length as Int).let { length ->
+        if (length < 1) {
+            return@let setOf<E>()
+        }
+
+        (0..length).map { array[it] as E }.toSet()
+    }
+
+    private fun put(key: String, value: Any) {
+        data[key] = value
+        fs.writeFileSync(userDataPath, JSON.stringify(data as Any))
+    }
+    private fun <T: Any> get(key: String): T? = data[key] as? T
+}
+
+
