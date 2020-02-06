c23078f94d0e60f3ba248f025a39fa9b2c89166f
subroh0508 <subroh.0508@gmail.com>
2020-02-06 23:33:24 +0900

    [edit]Change function to property

--- bridges/signin/src/jsMain/kotlin/id44/mizuki/bridges/signin/SignInNativeActions.kt
+++ bridges/signin/src/jsMain/kotlin/id44/mizuki/bridges/signin/SignInNativeActions.kt
@@ -8,16 +8,13 @@ import kotlin.js.Promise
 internal actual class SignInNativeActions : KodeinAware {
     override val kodein: Kodein = signInNativeActionsComponent()
 
-    fun startOauth2Flow(host: String): Promise<Unit> = Promise { resolve, reject ->
-        console.log("On Kotlin/JS! / host: $host")
-        resolve(Unit)
+    val startOauth2Flow: (String) -> Promise<Unit> = { host ->
+        Promise { resolve, reject ->
+            console.log("On Kotlin/JS! / host: $host")
+            resolve(Unit)
+        }
     }
 
-    fun showErrorMessage(message: String) {
-        console.log("On Kotlin/JS! / message: $message")
-    }
-
-    fun openTimeline() {
-        console.log("On Kotlin/JS!")
-    }
+    val showErrorMessage: (String) -> Unit = { message -> console.log("On Kotlin/JS! / message: $message") }
+    val openTimeline: () -> Unit = { console.log("On Kotlin/JS!") }
 }
