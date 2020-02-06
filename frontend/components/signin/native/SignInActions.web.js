5226a059400313ff4b7d7878930995126faaf884
subroh0508 <subroh.0508@gmail.com>
2020-02-07 00:28:09 +0900

    [delete]Delete SignInActions.js

--- frontend/components/signin/native/SignInActions.web.js
+++
@@ -1,3 +0,0 @@
-export const startOauth2Flow = (host) => { console.log(`call on web: ${host}`); };
-export const showToast = (message) => { console.log(`call on web: ${message}`); };
-export const openTimeline = () => {};
