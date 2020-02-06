2431136df8ae8dd9342cd30a1c3219564de14c79
subroh0508 <subroh.0508@gmail.com>
2020-02-06 23:33:49 +0900

    [edit]Use Kotlin/JS module on Electron

--- desktop/renderer/index.js
+++ desktop/renderer/index.js
@@ -1,6 +1,10 @@
-import React from 'react';
 import { AppRegistry } from 'react-native';
-import { registerComponent } from 'ID44-frontend/components/signin';
+import registerComponent from 'ID44-frontend/utilities/registerComponent';
+import { SignIn } from 'ID44-frontend/components/signin/containers/SignIn';
+import reducer from 'ID44-frontend/components/signin/reducers';
+import { SignInNativeActions } from 'ID44-bridges-signin';
 
-registerComponent();
+console.log('SignInNativeActions', SignInNativeActions);
+registerComponent('SignIn', SignIn, reducer, SignInNativeActions);
+
 AppRegistry.runApplication('SignIn', { rootTag: document.getElementById('app') });
