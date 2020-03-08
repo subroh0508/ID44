ce6c712d0d2c8b5ba27c868ff3ea173276349b6f
subroh0508 <subroh.0508@gmail.com>
2020-03-08 23:47:24 +0900

    [add]Add ReactNativeViewModel

---
+++ android/base/src/main/kotlin/id44/mizuki/base/reactnative/ReactNativeViewModel.kt
@@ -0,0 +1,10 @@
+package id44.mizuki.base.reactnative
+
+import androidx.lifecycle.ViewModel
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+
+class ReactNativeViewModel(
+    reactContextBaseJavaModule: ReactContextBaseJavaModule
+) : ViewModel(), NativeModule by reactContextBaseJavaModule {
+}
--- domain/signin/usecase-signin/src/commonMain/kotlin/id44/mizuki/domain/signin/usecase/di/modules.kt
+++ domain/signin/usecase-signin/src/commonMain/kotlin/id44/mizuki/domain/signin/usecase/di/modules.kt
@@ -4,12 +4,15 @@ import id44.mizuki.domain.signin.usecase.RequestAccessTokenUseCase
 import id44.mizuki.domain.signin.usecase.RequestAccessTokenUseCaseImpl
 import id44.mizuki.domain.signin.usecase.RequestAppCredentialUseCase
 import id44.mizuki.domain.signin.usecase.RequestAppCredentialUseCaseImpl
+import id44.mizuki.infra.auth.repository.di.authRepositoryModule
 import org.kodein.di.Kodein
 import org.kodein.di.erased.bind
 import org.kodein.di.erased.instance
 import org.kodein.di.erased.singleton
 
 val signInUseCaseModule = Kodein.Module(name = "SignInUseCaseModule") {
+    import(authRepositoryModule)
+
     bind<RequestAccessTokenUseCase>() with singleton { RequestAccessTokenUseCaseImpl(instance(), instance()) }
     bind<RequestAppCredentialUseCase>() with singleton { RequestAppCredentialUseCaseImpl(instance(), instance()) }
 }
