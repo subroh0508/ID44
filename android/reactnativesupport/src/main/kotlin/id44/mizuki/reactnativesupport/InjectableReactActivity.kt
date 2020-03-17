de4ca6b0b0c13a390e098701db5855762a54050d
subroh0508 <subroh.0508@gmail.com>
2020-03-18 00:45:55 +0900

    [add]Add InjectableReactActivity / SimpleReactNativeHost

---
+++ android/reactnativesupport/src/main/kotlin/id44/mizuki/reactnativesupport/InjectableReactActivity.kt
@@ -0,0 +1,18 @@
+package id44.mizuki.reactnativesupport
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactRootView
+import org.kodein.di.KodeinAware
+import org.kodein.di.generic.instance
+
+abstract class InjectableReactActivity : ReactActivity(), KodeinAware {
+    val host: ReactNativeHost by instance()
+    val reactRootView: ReactRootView by instance()
+
+    override fun createReactActivityDelegate() = object : ReactActivityDelegate(this, mainComponentName) {
+        override fun getReactNativeHost(): ReactNativeHost = host
+        override fun createRootView(): ReactRootView = reactRootView
+    }
+}
---
+++ android/reactnativesupport/src/main/kotlin/id44/mizuki/reactnativesupport/SimpleReactNativeHost.kt
@@ -0,0 +1,38 @@
+package id44.mizuki.reactnativesupport
+
+import android.app.Application
+import android.view.View
+import androidx.annotation.CallSuper
+import com.dylanvann.fastimage.FastImageViewPackage
+import com.facebook.react.ReactInstanceManager
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.shell.MainReactPackage
+import com.facebook.react.uimanager.ReactShadowNode
+import com.facebook.react.uimanager.ViewManager
+
+class SimpleReactNativeHost(
+    app: Application,
+    private val bundleName: String, private val jsModulePath: String,
+    private val nativeModules: List<NativeModule> = listOf(),
+    private val viewManagers: List<ViewManager<View, ReactShadowNode<*>>> = listOf(),
+    private val packages: List<ReactPackage> = listOf()
+) : ReactNativeHost(app) {
+    private val featurePackage = object : ReactPackage {
+        override fun createViewManagers(reactContext: ReactApplicationContext) = viewManagers
+        override fun createNativeModules(reactContext: ReactApplicationContext) = nativeModules
+    }
+
+    override fun getPackages() = listOf(MainReactPackage(), featurePackage) + packages
+
+    override fun getBundleAssetName() = bundleName
+    override fun getJSMainModuleName() = jsModulePath
+
+    override fun getUseDeveloperSupport() = BuildConfig.DEBUG
+    override fun createReactInstanceManager(): ReactInstanceManager =
+        super.createReactInstanceManager().apply {
+            devSupportManager.setReloadOnJSChangeEnabled(BuildConfig.DEBUG)
+        }
+}
